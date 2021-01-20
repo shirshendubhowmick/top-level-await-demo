fcd85cbe74e879d9660c6c68be8409cc02f79338
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-01-21 02:42:41 +0530

    top level await demo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import awaitinModule from './awaitingModule'
+import awaitingModule from './awaitingModule'
+import normalModule from './normalModule';
+import qs from 'qs';
 
-console.log(awaitinModule);
+console.log(awaitingModule);
+console.log("Hello from index.js")
+console.log(normalModule);
+
+const queryObj = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+
+const selectedLanguage = queryObj.language || 'english';
+
+document.getElementById("fetch-button").addEventListener("click", function(event) {
+  console.log("Clicked button");
+});
+
+const greetings = await import(`./languageData/${selectedLanguage}.js`);
+document.getElementById("greetings").innerText=greetings.default;
+console.log("Language file loaded");
