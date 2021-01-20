7ea680ddfa81e7732886cc0f6f24866a561f8565
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-01-21 00:29:39 +0530

    added regenator runtime

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
 
   mode: 'development',
-  entry: './src/index.js',
+  entry: ['regenerator-runtime', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: '[name].bundle.js',
