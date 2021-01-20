f8aaa2d9282e08332fd364fd32c356300a90a80c
Shirshendu Bhowmick <shirshendu.bhowmick3@gmail.com>
2021-01-20 22:08:28 +0530

    added an awaited module

--- src/awaitingModule/index.js
+++ src/awaitingModule/index.js
@@ -2,8 +2,11 @@ const awaitingModule = new Promise((resolve) => {
   setTimeout(() => resolve("Resolved"), 10000)
 })
 
+console.log("Will start to await");
+
 const value = await awaitingModule;
 
-console.log("Test");
+console.log("Awaiting complete");
+
 
 export default value;
